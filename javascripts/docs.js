8f2b59a62366ad81a0cb01280994169c0daf7f0f
mlbli <hello@mlb.li>
2012-11-09 23:55:49 +0100

    meta description

--- javascripts/docs.js
+++ javascripts/docs.js
@@ -131,7 +131,7 @@ Element.ready(function(){
   })
   
   Element.create("script", {
-    src : "data/data.minified.js",
+    src : "data/data-minified.js",
     type : "text/javascript"
   }).appendTo(document.body)
   
