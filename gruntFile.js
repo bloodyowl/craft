ad5fb96d08f0e06c82c4f64eb6577c85dd79c459
mlbli <hello@mlb.li>
2013-04-21 00:57:33 +0200

    2.0.9, improved Function.prototype.attach behaviour when called without arguments in the first place

--- gruntFile.js
+++ gruntFile.js
@@ -7,7 +7,7 @@ module.exports = function(grunt){
 
   grunt.initConfig({
       meta : {
-        banner : "/*!\n  Craft.js\n  2.0.8 \n*/"
+        banner : "/*!\n  Craft.js\n  2.0.9 \n*/"
       }
     , concat : {
           dist : {
